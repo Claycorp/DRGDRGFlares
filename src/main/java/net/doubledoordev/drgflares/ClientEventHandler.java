3b6b865d9c88561448742b3253c25ea5480b93fc
Colby Prince <daemon.umbra@gmail.com>
2021-10-02 23:56:04 -0400

    Fixed Inoperable Mod
    Fixed some event handlers that weren't being invoked

--- src/main/java/net/doubledoordev/drgflares/ClientEventHandler.java
+++ src/main/java/net/doubledoordev/drgflares/ClientEventHandler.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import org.lwjgl.glfw.GLFW;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
@@ -17,7 +18,6 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DeferredWorkQueue;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.network.PacketDistributor;
 
@@ -26,45 +26,47 @@ import net.doubledoordev.drgflares.capability.FlareProvider;
 import net.doubledoordev.drgflares.networking.PacketHandler;
 import net.doubledoordev.drgflares.networking.ThrowFlarePacket;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+
 public class ClientEventHandler
 {
     // Keybind for Flares
     public static final KeyBinding THROW_FLARE = new KeyBinding("drgflares.key.throwflare", KeyConflictContext.IN_GAME, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_V, "Flares");
 
-    public static PlayerEntity getPlayer()
-    {
+    public static PlayerEntity getPlayer() {
         return Minecraft.getInstance().player;
     }
 
-    @SubscribeEvent
-    public static void clientRendering(FMLClientSetupEvent event)
-    {
-        ClientRegistry.registerKeyBinding(THROW_FLARE);
-        RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.FLARE_ENTITY.get(), manager -> new SpriteRenderer<>(event.getMinecraftSupplier().get().getEntityRenderDispatcher(), event.getMinecraftSupplier().get().getItemRenderer()));
-        DeferredWorkQueue.runLater(() -> RenderTypeLookup.setRenderLayer(BlockRegistry.FAKE_LIGHT.get(), RenderType.cutout()));
-    }
+    @EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+    public static class Mod {
 
-    @SubscribeEvent
-    public void drawTextEvent(RenderGameOverlayEvent.Text event)
-    {
-        PlayerEntity player = Minecraft.getInstance().player;
-
-        if (DRGFlaresConfig.GENERAL.displayFlareCount.get() && player != null)
-        {
-            player.getCapability(FlareProvider.FLARE_CAP_CAPABILITY).ifPresent(flareCap -> {
-                event.getLeft().add("Flares: " + flareCap.getStoredFlares() + "/" + DRGFlaresConfig.GENERAL.flareQuantity.get());
-            });
+        @SubscribeEvent
+        public static void clientRendering(FMLClientSetupEvent event) {
+            ClientRegistry.registerKeyBinding(THROW_FLARE);
+            RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.FLARE_ENTITY.get(), manager -> new SpriteRenderer<>(event.getMinecraftSupplier().get().getEntityRenderDispatcher(), event.getMinecraftSupplier().get().getItemRenderer()));
+            DeferredWorkQueue.runLater(() -> RenderTypeLookup.setRenderLayer(BlockRegistry.FAKE_LIGHT.get(), RenderType.cutout()));
         }
     }
 
-    // Catch our key press, fire off a packet to the server for handling.
-    @SubscribeEvent
-    public void onKeyEvent(InputEvent.KeyInputEvent event)
-    {
-        if (THROW_FLARE.isDown())
-        {
-            PacketHandler.send(PacketDistributor.SERVER.noArg(), new ThrowFlarePacket());
+    @EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.FORGE)
+    public static class Forge {
+
+        // Catch our key press, fire off a packet to the server for handling.
+        @SubscribeEvent
+        public static void onKeyEvent(InputEvent.KeyInputEvent event) {
+            if (THROW_FLARE.isDown()) {
+                PacketHandler.send(PacketDistributor.SERVER.noArg(), new ThrowFlarePacket());
+            }
+        }
+
+        @SubscribeEvent
+        public static void drawTextEvent(RenderGameOverlayEvent.Text event) {
+            PlayerEntity player = Minecraft.getInstance().player;
+
+            if (DRGFlaresConfig.GENERAL.displayFlareCount.get() && player != null) {
+                player.getCapability(FlareProvider.FLARE_CAP_CAPABILITY).ifPresent(flareCap -> {
+                    event.getLeft().add("Flares: " + flareCap.getStoredFlares() + "/" + DRGFlaresConfig.GENERAL.flareQuantity.get());
+                });
+            }
         }
     }
 }
